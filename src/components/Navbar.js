c6ff8b9bb635ffbc3b3d440e7799a8d8cf98a149
innotuyi <innotuyi67@gmail.com>
2023-11-23 16:46:44 +0200

    fixing conflicts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,18 @@
 import React from "react";
-import { Link , useHistory} from "react-router-dom";
-import profile from '../image/profile.png'
+import { Link, useHistory } from "react-router-dom";
+import profile from "../image/profile.png";
 const Navbar = () => {
-
   const token = localStorage.getItem("token");
 
   const role = localStorage.getItem("role");
 
-
   const history = useHistory();
-
 
   function handleLogout() {
     localStorage.removeItem("token");
     localStorage.removeItem("isEnrolled");
     history.push("/");
-    window.location.reload()
+    window.location.reload();
   }
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -41,63 +38,57 @@ const Navbar = () => {
             </Link>
           </li>
           <li class="nav-item">
-            <Link class="nav-link" to="/pricing">
-              Pricing
+            <Link class="nav-link" to="/course">
+              Course
             </Link>
           </li>
         </ul>
-        <ul class="navbar-nav ml-auto">        
-  {token ? (
-   <li class="nav-item dropdown">
-   <a
-     class="nav-link dropdown-toggle"
-     href="#"
-     role="button"
-     data-toggle="dropdown"
-     aria-haspopup="true"
-     aria-expanded="false"
-   >
-     <img
-       src={profile}
-       alt="Profile Photo"
-       class="profile-photo"
-     />
-     <span class="profile-name">{role}</span> 
-   </a>
-   <div class="dropdown-menu">
-     {/* <Link class="dropdown-item" to="/profile">
+        <ul class="navbar-nav ml-auto">
+          {token ? (
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <img src={profile} alt="Profile Photo" class="profile-photo" />
+                <span class="profile-name">{role}</span>
+              </a>
+              <div class="dropdown-menu">
+                {/* <Link class="dropdown-item" to="/profile">
        My Profile
      </Link>
      <Link class="dropdown-item" href="/settings">
        Settings
      </Link> */}
-     <Link class="dropdown-item" onClick={handleLogout}>
-       Logout
-     </Link>
-   </div>
- </li>
- 
-  ) : (
-    <>
-      <li class="nav-item">
-        <Link class="nav-link" to="/about">
-          About
-        </Link>
-      </li>
-      <li class="nav-item">
-        <Link class="nav-link" to="/contact">
-          Contact
-        </Link>
-      </li>
-      <li class="nav-item">
-        <Link class="nav-link" to="/register">
-          Login/Register
-        </Link>
-      </li>
-    </>
-  )}
-</ul>
-
+                <Link class="dropdown-item" onClick={handleLogout}>
+                  Logout
+                </Link>
+              </div>
+            </li>
+          ) : (
+            <>
+              <li class="nav-item">
+                <Link class="nav-link" to="/about">
+                  About
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" to="/register">
+                  Login/Register
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
       </div>
     </nav>
   );
